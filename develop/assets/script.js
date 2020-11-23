474e238bfc270458edaab0a3e7026e9156e2c5c9
Charles Latrobe <Charlie.bateman117@gmail.com>
2020-11-23 12:42:48 -0800

    messed around with making DOM els.

---
+++ develop/assets/script.js
@@ -0,0 +1,8 @@
+// bio info
+
+var container = document.getElementById("container-1");
+var bioDiv = document.createElement("div");
+bioDiv.setAttribute("class", "row");
+bioDiv.style.textAlign= 'center';
+bioDiv.textContent = "Welcome to Lia Liu Chinese"
+container.appendChild(bioDiv);
