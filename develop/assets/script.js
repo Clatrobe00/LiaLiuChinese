4ebd7c32f8a6989dea12623755f6afe8f176eaa3
Charles Latrobe <Charlie.bateman117@gmail.com>
2020-12-09 12:42:21 -0800

    reformat code into single HTML page

--- develop/assets/script.js
+++
@@ -1,8 +0,0 @@
-// bio info
-
-var container = document.getElementById("container-1");
-var bioDiv = document.createElement("div");
-bioDiv.setAttribute("class", "row");
-bioDiv.style.textAlign= 'center';
-bioDiv.textContent = "Welcome to Lia Liu Chinese"
-container.appendChild(bioDiv);
